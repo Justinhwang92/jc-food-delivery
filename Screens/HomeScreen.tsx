8347f06dc384a4c4fed9f09a5e3d0516df653636
justinhwang92 <justinhwang92@gmail.com>
2023-01-17 21:48:43 -0500

    Setup project and install dependencies

---
+++ Screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { styled } from 'nativewind';
+
+const HomeScreen = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-red-800">HomeScreen</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
